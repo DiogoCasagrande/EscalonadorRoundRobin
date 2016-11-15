12f74c2512135e89eb42cf26844ac384d2bd4d16
DiogoCasagrande <casagrande.diogo@gmail.com>
2016-11-15 19:42:23 -0200

    Update Processo.java

--- Processo.java
+++ Processo.java
@@ -1,13 +1,15 @@
 public class Processo {
-    String pid;
-    int duracao;
-    int chegada;
-    int opIO [];
+    public String pid;
+    public int duracao;
+    public int chegada;
+    public int opIO [];
+    public int cpuUsing;
     
     public Processo(String pid, int duracao,int chegada,int opIO[]){
         this.pid = pid;
         this.duracao = duracao;
         this.chegada = chegada;
         this.opIO = opIO;
+        this.cpuUsing = 0;
     }
 }
