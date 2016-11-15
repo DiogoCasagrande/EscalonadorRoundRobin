8100db58450586f2e01701a811747e4274e44214
DiogoCasagrande <casagrande.diogo@gmail.com>
2016-11-15 16:57:51 -0200

    Update FilaPronto.java

--- FilaPronto.java
+++ FilaPronto.java
@@ -1,5 +1,47 @@
+//implementação da fila
 public class FilaPronto {
-    //fila de processos[]
-    //dequeue
-    //equeue
+    noFilaCircular inicio, fim, proximo;
+    
+    public boolean vazia(){
+        return inicio == null && fim == null;
+    }//caso a fila esteja vazia retorna verdadeiro
+    
+    public void adicionarFila(int dado){
+        noFilaCircular novo = new noFilaCircular(dado);
+        
+        if(vazia()){
+            inicio = novo;
+            fim = novo;
+            fim.proximo = inicio;
+        }else{
+            novo.proximo = inicio;
+            fim.proximo = novo;
+            fim = novo;
+        }
+    }
+    
+    public int removerFila(){
+        int removido;
+        
+        if (vazia()){
+            removido = -1;
+        }else if (inicio == fim){
+            removido = inicio.dado;
+            inicio = null;
+            fim = null;
+        }else{
+            removido = inicio.dado;
+            fim = inicio;
+            inicio = inicio.proximo;
+            fim.proximo = inicio;
+        }
+        return removido;
+    }
+    
+    public FilaPronto(){
+        this.inicio = null;
+        this.fim = null;
+        
+    }
+    
 }
